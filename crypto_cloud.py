b58ee4d18a61df397222854e3cb45419434c190c
Jiexi Cao <jiexicao@gmail.com>
2015-05-06 20:50:59 +0200

    Init

--- crypto_cloud.py
+++ crypto_cloud.py
@@ -6,17 +6,22 @@ import secret
 from azure.storage import BlobService
 
 def main(myopts):
+    file_name = ""
+    container_name = ""
     for opt, obj in myopts:
         if opt == "-u":
-            print("You want to upload %s" % obj)
-            upload_file(obj)
+            file_name = obj
+        elif opt == "-c":
+            container_name = obj
         elif opt == "-l":
-            print("You want to list all the files")
+            print "You want to list all the files"
             # Call option to list all files
-        elif oopt = "-h":
+        elif opt == "-h":
             print "-h \t Help Information." + \
                     "\n -u \t Filename of file to upload." + \
-                    "\n -c \t Filename of container to add to." +  \
+                    "\n -c \t Filename of container to add to."
+    print "You want to upload %s to container %s" % (file_name, container_name)
+    upload_file(file_name, container_name)
 
 def upload_file(file_name, container_name):
     blob_service = BlobService(account_name=secret.STORAGE_ACCOUNT_NAME,
@@ -24,5 +29,5 @@ def upload_file(file_name, container_name):
     blob_service.create_container(container_name)
 
 if __name__ == "__main__":
-    myopts, args = getopt.getopt(sys.argv[1:],"u:e:")
+    myopts, args = getopt.getopt(sys.argv[1:],"u:c:l:h")
     main(myopts)
