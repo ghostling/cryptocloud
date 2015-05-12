51c385ff02711e5dc2f2f36effa083f629fdedf5
Jiexi Cao <jiexicao@gmail.com>
2015-05-13 00:58:37 +0200

    Fixed stupid auth bug, added comments, and completed help text

--- crypto_cloud.py
+++ crypto_cloud.py
@@ -5,25 +5,28 @@ import os, sys, getopt
 import secret
 import mimetypes
 from azure.storage import BlobService
+from azure.http import ( HTTPError, HTTPRequest )
 import cryptography.hazmat.backends as b
 from cryptography.hazmat.primitives.ciphers import (
     Cipher, algorithms, modes
 )
 
 IV_LEN = 12
+TAG_LEN = 16
 key = open("key.txt","r").read()
+container_name = "testcontainer"
 
 blob_service = BlobService(account_name=secret.STORAGE_ACCOUNT_NAME,
         account_key=secret.PRIMARY_ACCESS_KEY)
-container_name = "testcontainer"
 
 def main(myopts):
-    blob_service.set_container_acl(container_name=container_name,
-            x_ms_blob_public_access='container')
-
     file_name = ""
     function = ""
 
+    if not myopts:
+        print_help_commands()
+
+    # Parses commands given.
     for opt, obj in myopts:
         if opt == "-u":
             file_name = obj
@@ -33,11 +36,10 @@ def main(myopts):
             function = "download"
         elif opt == "-l":
             print "You want to list all the files"
-            # Call option to list all files
+            # TODO: Call option to list all files
         elif opt == "-h":
-            print "-h \t Help Information." + \
-                    "\n -u \t Filename of file to upload." + \
-                    "\n -c \t Filename of container to add to."
+            print_help_commands()
+
     if function == "upload":
         print "You want to upload " + file_name
         upload_file(file_name)
@@ -45,21 +47,25 @@ def main(myopts):
         print "You want to download " + file_name
         download_file(file_name)
 
-    blob_service.set_container_acl(container_name=container_name,
-            x_ms_blob_public_access=None)
+def print_help_commands():
+    # Shows users commands accepted.
+    print "\n=== Help Information ===" + \
+            "\n -u [filename]  \t Uploads & encrypts specified file." + \
+            "\n -d [filename]  \t Downloads & decrypts specified file.\n"
 
+# Note: We wanted to use blob_service.get_blob_to_path(...) originally to
+# skip the intermediary step of saving it to a "cipher_out.txt" file, but that
+# function is broken in the API.
 def download_file(file_name):
     cipher_text = ""
-    blob_service.get_blob_to_bytes(container_name, file_name, cipher_text)
+    blob_service.get_blob_to_path(container_name, file_name, 'cipher_out.txt')
+    cipher_text = open('cipher_out.txt', 'r').read()
     decrypted = decrypt_file(cipher_text)
     new_file = open("downloaded_"+file_name, "w")
     new_file.write(decrypted)
     new_file.close()
 
 def upload_file(file_name):
-    if container_name not in blob_service.list_containers():
-        blob_service.create_container(container_name)
-
     file_type = mimetypes.guess_type(file_name)[0]
     encrypted_file = encrypt_file(file_name)
     blob_service.put_block_blob_from_bytes(
@@ -69,15 +75,20 @@ def encrypt_file(file_name):
     iv = os.urandom(IV_LEN)
     file_bytes = open(file_name, "rb").read()
     encryptor = Cipher(
-        algorithms.AES(key), modes.GCM(iv), backend=b.default_backend()).encryptor()
-    cipher_text = iv + encryptor.update(file_bytes) + encryptor.finalize()
+        algorithms.AES(key),
+        modes.GCM(iv),
+        backend=b.default_backend()).encryptor()
+    cipher_text = encryptor.update(file_bytes) + encryptor.finalize()
+    cipher_text = iv + encryptor.tag + cipher_text
     return cipher_text
 
 def decrypt_file(cipher_text):
     iv = cipher_text[:IV_LEN]
-    decryptor = Cipher(algorithms.AES(key), modes.GCM(iv), backend=b.default_backend()).decryptor()
-
-    return decryptor.update(cipher_text) + decryptor.finalize()
+    decryptor = Cipher(
+            algorithms.AES(key),
+            modes.GCM(iv, cipher_text[IV_LEN:IV_LEN+TAG_LEN]),
+            backend=b.default_backend()).decryptor()
+    return decryptor.update(cipher_text[IV_LEN+TAG_LEN:]) + decryptor.finalize()
 
 if __name__ == "__main__":
     myopts, args = getopt.getopt(sys.argv[1:],"u:d:c:l:h")
